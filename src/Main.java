a97c1fb78b988c8bcb9a1220c9e73f54bf0bb36f
sachko65 <levchencow@gmail.com>
2023-11-09 15:05:08 +0200

    test1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,36 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner in = new Scanner(System.in);
+        int n;
+        do {
+            System.out.print("Введіть номер завдання: ");
+            n = in.nextInt();
+            switch (n){
+                case 1 -> Task1();
+                //case 2 -> Task2();
+            }
+            System.out.println();
+        } while(n != 0);
+    }
+    static void Task1(){
+        for (double i = -1; i < 1; i += 0.2) {
+            System.out.print(i + "\t");
         }
     }
+    static void Task2(){
+        double sum = 0, num;
+        int count = 0;
+        Scanner in = new Scanner(System.in);
+        do {
+            num = in.nextDouble();
+            if (num > 0) sum += num;
+            else if (num < 0) count++;
+        } while(num != 0);
+        System.out.println("Сума додатніх: " + sum);
+        System.out.println("Кількість від'ємних" + count);
+    }
+
+
 }
